3f78a2bec6a92dc3ade1261089234cbfd9e3610c
Michal Madej <michal.madej@zf.com>
2022-09-04 19:19:16 +0200

    Added Test project

---
+++ KanbanBoard.Tests/Mocks/MockRepositories.cs
@@ -0,0 +1,39 @@
+﻿using KanbanBoard.Models;
+using KanbanBoard.Repositories;
+using KanbanBoard.Services.Interfaces;
+using Moq;
+
+namespace KanbanBoard.Tests.Mocks
+{
+    public static class MockRepositories
+    {
+        public static Mock<IKanbanService> GetKanbanRepository()
+        {
+            var kanbanItems = new List<ToDo>()
+            {
+                new ToDo()
+                {
+                    Id = 10,
+                    Name = "Create Tests",
+                    Status = Enums.StatusType.InProgress
+                },
+                new ToDo()
+                {
+                    Id = 20,
+                    Name = "Validate",
+                    Status = Enums.StatusType.ToDo
+                }
+            };
+
+            var mockRepo = new Mock<IKanbanService>();
+            mockRepo.Setup(r => r.GetAllTasks()).ReturnsAsync(kanbanItems);
+
+            mockRepo.Setup(r => r.AddToDo(It.IsAny<string>())).ReturnsAsync((int idOfTask) =>
+            {
+                return idOfTask;
+            });
+
+            return mockRepo;
+        }
+    }
+}
