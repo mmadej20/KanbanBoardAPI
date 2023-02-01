61084157ed307dce41bbfdba899a433214ea58bb
Michał Madej <dziadekdeamon@gmail.com>
2023-02-01 19:07:29 +0100

    Create BoardService with database calls

---
+++ KanbanBoard/Services/BoardService.cs
@@ -0,0 +1,138 @@
+﻿using AutoMapper.Execution;
+using DataAccess;
+using DataAccess.Models;
+using KanbanBoard.Domain;
+using KanbanBoard.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Services;
+
+public class BoardService : IBoardService
+{
+    private readonly KanbanContext _kanbanContext;
+
+    public BoardService(KanbanContext kanbanContext)
+    {
+        _kanbanContext = kanbanContext;
+    }
+
+    public async Task<OperationResult> AddMemberToBoard(int boardId, int memberId)
+    {
+        var boardToUpdate = await GetBoard(boardId);
+
+        if (boardToUpdate == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no board with id '{boardId}'" };
+
+        var memberToAdd = await _kanbanContext.Members.FindAsync(memberId);
+
+        if (memberToAdd == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no member with id '{memberId}'" };
+
+        boardToUpdate.Members.Add(memberToAdd);
+
+        if (await UpdateBoard(boardToUpdate) > 0)
+            return new OperationResult { IsSuccesfull = true, Message = "Member has beend added" };
+
+        return new OperationResult { IsSuccesfull = false, Message = "There is a problem with your request" };
+    }
+
+    public async Task<OperationResult> CreateBoard(string name)
+    {
+        await _kanbanContext.Boards.AddAsync(new Board { Name = name });
+
+        var affectedEntries = await _kanbanContext.SaveChangesAsync();
+
+        if (affectedEntries > 0)
+            return new OperationResult { IsSuccesfull = true, Message = $"Board '{name}' has been created" };
+
+        return new OperationResult { IsSuccesfull = false, Message = "There is a problem with your request" };
+    }
+
+    public async Task<OperationResult> DeleteBoard(int boardId)
+    {
+        var boardToRemove = await GetBoard(boardId);
+
+        if (boardToRemove == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no board with id '{boardId}'" };
+
+        _kanbanContext.Boards.Remove(boardToRemove);
+
+        var affectedEntries = await _kanbanContext.SaveChangesAsync();
+
+        if (affectedEntries > 0)
+            return new OperationResult { IsSuccesfull = true, Message = $"Board '{boardToRemove.Name}' has been removed" };
+
+        return new OperationResult { IsSuccesfull = false, Message = "There is a problem with your request" };
+    }
+
+    public async Task<OperationResult> RemoveMemberFromBoard(int boardId, int memberId)
+    {
+        var boardToUpdate = await GetBoard(boardId);
+
+        if (boardToUpdate == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no board with id '{boardId}'" };
+
+        var memberToRemove = await _kanbanContext.Members.FindAsync(memberId);
+
+        if (memberToRemove == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no member with id '{memberId}'" };
+
+        var removeResult = boardToUpdate.Members.Remove(memberToRemove);
+
+        if (!removeResult)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no member with id '{memberId}' related to this board!" };
+
+        if (await UpdateBoard(boardToUpdate) > 0)
+            return new OperationResult { IsSuccesfull = true, Message = "Member has been removed from board" };
+
+        return new OperationResult { IsSuccesfull = false, Message = "There is a problem with your request" };
+    }
+
+    public async Task<OperationResult> UpdateBoardName(int boardId, string newName)
+    {
+        var boardToUpdate = await GetBoard(boardId);
+
+        if (boardToUpdate == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no board with id '{boardId}'" };
+
+        boardToUpdate.Name = newName;
+
+        if (await UpdateBoard(boardToUpdate) > 0)
+            return new OperationResult { IsSuccesfull = true, Message = "Member has been removed from board" };
+
+        return new OperationResult { IsSuccesfull = false, Message = "There is a problem with your request" };
+    }
+
+    public async Task<OperationResult> AddItemToBoard(int boardId, int itemId)
+    {
+        var boardToUpdate = await GetBoard(boardId);
+
+        if (boardToUpdate == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no board with id '{boardId}'" };
+
+        var itemToAdd = await _kanbanContext.ToDos.FindAsync(itemId);
+
+        if (itemToAdd == null)
+            return new OperationResult { IsSuccesfull = false, Message = $"There is no item with id '{itemId}'" };
+
+        boardToUpdate.ToDoItems.Add(itemToAdd);
+
+        if (await UpdateBoard(boardToUpdate) > 0)
+            return new OperationResult { IsSuccesfull = true, Message = "Item has been added to board" };
+
+        return new OperationResult { IsSuccesfull = false, Message = "There is a problem with your request" };
+    }
+
+    public async Task<Board> GetBoard(int boardId) => await _kanbanContext.Boards.FindAsync(boardId);
+
+    /// <returns>Affected entries of update command</returns>
+    private async Task<int> UpdateBoard(Board boardToUpdate)
+    {
+        _kanbanContext.Boards.Update(boardToUpdate);
+
+        return await _kanbanContext.SaveChangesAsync();
+    }
+
+}
---
+++ KanbanBoard/Services/Interfaces/IBoardService.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Models;
+using KanbanBoard.Domain;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Services.Interfaces;
+
+public interface IBoardService
+{
+    /// <summary>
+    /// Create board with given name
+    /// </summary>
+    /// <param name="name">New board name</param>
+    /// <returns>Result of an operation</returns>
+    Task<OperationResult> CreateBoard(string name);
+
+    /// <summary>
+    /// Adds ToDo item to board
+    /// </summary>
+    /// <param name="boardId">ID of board where item is going to be added</param>
+    /// <param name="itemId">ID of ToDo item</param>
+    /// <returns></returns>
+    Task<OperationResult> AddItemToBoard(int boardId, int itemId);
+
+    /// <summary>
+    /// Get board with members and tasks
+    /// </summary>
+    /// <param name="boardId">ID of board</param>
+    /// <returns>Board with members and tasks</returns>
+    Task<Board> GetBoard(int boardId);
+
+    /// <summary>
+    /// Change board name to new one
+    /// </summary>
+    /// <param name="boardId">ID of board to update</param>
+    /// <param name="newName">New name of board</param>
+    /// <returns>Result of an operation</returns>
+    Task<OperationResult> UpdateBoardName(int boardId, string newName);
+
+    /// <summary>
+    /// Delete board with tasks
+    /// </summary>
+    /// <param name="boardId">ID of board to be removed</param>
+    /// <returns>Result of an operation</returns>
+    Task<OperationResult> DeleteBoard(int boardId);
+
+    /// <summary>
+    /// Adds member to board
+    /// </summary>
+    /// <param name="boardId">ID of board where new member is going to be add</param>
+    /// <param name="memberId">ID of new member</param>
+    /// <returns>Result of an operation</returns>
+    Task<OperationResult> AddMemberToBoard(int boardId, int memberId);
+
+    /// <summary>
+    /// Removes member from board and all task assigned to him
+    /// </summary>
+    /// <param name="boardId">ID of board from member will be removed</param>
+    /// <param name="memberId">ID of existing member</param>
+    /// <returns>Result of an operation</returns>
+    Task<OperationResult> RemoveMemberFromBoard(int boardId, int memberId);
+
+
+}
