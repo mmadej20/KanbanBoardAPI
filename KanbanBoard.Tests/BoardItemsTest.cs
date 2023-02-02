148a0afb4a3153692601fe07487b4a04d834ac7d
Michal Madej <michal.madej@zf.com>
2023-02-02 13:17:06 +0100

    Add database fixture and tests using it

---
+++ KanbanBoard.Tests/BoardItemsTest.cs
@@ -0,0 +1,44 @@
+using DataAccess.Enums;
+using DataAccess.Models;
+using KanbanBoard.Services;
+using KanbanBoard.Services.Interfaces;
+using KanbanBoard.Tests.DatabaseFixture;
+using Moq;
+
+namespace KanbanBoard.Tests
+{
+    public class BoardItemsTest : IClassFixture<KanbanDatabaseFixture>
+    {
+        private readonly BoardItemService _mockService;
+
+        private readonly BoardItemService _boardItemService;
+
+        public BoardItemsTest(KanbanDatabaseFixture fixture)
+        {
+            _mockService = Mocks.MockRepositories.GetToDoService();
+            _boardItemService = FixtureDatabase.GetBoardItemServiceWithFixtureDatabase(fixture);
+        }
+
+        [Fact]
+        public void TaskShouldBeAddedToRepository()
+        {
+            var result = _boardItemService.AddToDo("TestTask");
+
+            Assert.True(result.Result.IsSuccesfull);
+        }
+
+        [Fact]
+        public void GetAllTasksShouldReturnListOfToDo()
+        {
+            Assert.IsType<List<ToDo>>(_boardItemService.GetAllTasks().Result);
+        }
+
+        [Fact]
+        public void StatusShouldBeChangeToInProgress()
+        {
+            _boardItemService.ChangeStatus(1, StatusType.InProgress).GetAwaiter();
+            var inProgressTask = _boardItemService.GetToDoById(1).Result;
+            Assert.StartsWith("InProgress", inProgressTask.Status.ToString());
+        }
+    }
+}
---
+++ KanbanBoard.Tests/DatabaseFixture/KanbanDatabseFixture.cs
@@ -0,0 +1,59 @@
+﻿using DataAccess;
+using DataAccess.Enums;
+using DataAccess.Models;
+using KanbanBoard.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.Tests.DatabaseFixture
+{
+    public class KanbanDatabaseFixture
+    {
+        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=KanbanTestDatabase;Trusted_Connection=True";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public KanbanDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+                        context.AddRange(
+                        new ToDo { Name = "Task432", Status = StatusType.ToDo},
+                            new ToDo { Name = "Task999", Status = StatusType.OnHold });
+                        context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public KanbanContext CreateContext()
+            => new KanbanContext(
+                new DbContextOptionsBuilder<KanbanContext>()
+                    .UseSqlServer(ConnectionString)
+                    .Options);
+    }
+
+    public static class FixtureDatabase
+    {
+        public static BoardItemService GetBoardItemWithFixtureDatabase(KanbanDatabaseFixture fixture)
+        {
+            var mockRepo = new BoardItemService(fixture.CreateContext());
+
+            return mockRepo;
+        }
+    }
+}
