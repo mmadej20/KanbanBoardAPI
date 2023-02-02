60d0fca72c0b46f2e3c8915797753d57de0cbb58
Michał Madej <dziadekdeamon@gmail.com>
2023-02-02 20:19:55 +0100

    Add maping from board to response

---
+++ KanbanBoard/Maps/BoardToResponse.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DataAccess.Models;
+using static KanbanBoard.Queries.Boards.GetBoardById;
+
+namespace KanbanBoard.Maps
+{
+    public class BoardToResponse : Profile
+    {
+        public BoardToResponse() 
+        {
+            CreateMap<Board, Response>();
+        }
+    }
+}
