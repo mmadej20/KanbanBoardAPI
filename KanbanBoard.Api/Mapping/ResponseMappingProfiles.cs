1403b604368d6f991e33785c8d6d16f2a4d6ab15
Michał Madej <madejmichal20@gmail.com>
2025-10-19 11:32:32 +0200

    Add response mapping profiles

---
+++ KanbanBoard.Api/Mapping/ResponseMappingProfiles.cs
@@ -0,0 +1,44 @@
+﻿using KanbanBoard.Api.Contracts.Board.Responses;
+using KanbanBoard.Api.Contracts.BoardItem.Responses;
+using KanbanBoard.Api.Contracts.Member.Responses;
+using KanbanBoard.Domain.Entities;
+using System.Linq;
+
+namespace KanbanBoard.Api.Mapping
+{
+    public static class ResponseMappingProfiles
+    {
+        public static BoardResponse ToBoardResponse(this Board dto)
+            => new()
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                ToDoItems = (dto.BoardItems ?? Enumerable.Empty<BoardItem>())
+                              .Select(t => t.ToBoardItemResponse())
+                              .ToList()
+                              .AsReadOnly(),
+                Members = (dto.Members ?? Enumerable.Empty<Member>())
+                              .Select(m => m.ToMemberResponse())
+                              .ToList()
+                              .AsReadOnly()
+            };
+
+        public static BoardItemResponse ToBoardItemResponse(this BoardItem dto)
+            => new()
+            {
+                Id = dto.Id,
+                Name = dto.Name,
+                Status = dto.Status,
+                AssignedMemberId = dto.AssignedMemberId,
+                BoardId = dto.BoardId
+            };
+
+        public static MemberResponse ToMemberResponse(this Member dto)
+            => new()
+            {
+                Id = dto.Id,
+                MemberName = dto.MemberName,
+                Email = dto.Email
+            };
+    }
+}
