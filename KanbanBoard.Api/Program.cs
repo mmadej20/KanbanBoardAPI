27c3377ee934ae9797d598cc66396f67e10246f9
Michał Madej <madejmichal20@gmail.com>
2025-10-19 17:26:59 +0200

    Add docker support

--- KanbanBoard.Api/Program.cs
+++ KanbanBoard.Api/Program.cs
@@ -1,11 +1,11 @@
+using KanbanBoard.DataAccess;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace KanbanBoard.Api;
 
@@ -13,7 +13,43 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var host = CreateHostBuilder(args).Build();
+
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            try
+            {
+                var context = services.GetRequiredService<KanbanContext>();
+
+                var retry = 0;
+                var maxRetry = 10;
+                while (retry < maxRetry)
+                {
+                    try
+                    {
+                        logger.LogInformation("Applying database migrations if database does not exist");
+                        context.Database.Migrate();
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        retry++;
+                        logger.LogWarning("Database not ready yet - {Retry}/{MaxRetry}", retry, maxRetry);
+                        Thread.Sleep(2500);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error during database initialization");
+                throw;
+            }
+        }
+
+        host.Run();
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
